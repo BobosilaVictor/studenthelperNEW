190032df34311b188f00b08184ad9da661b099a8
BobosilaVictor <vbobosila1@gmail.com>
2019-11-23 16:50:08 +0200

    TETST

--- app/src/main/java/com/example/studenthelpernew/SessionHandler.java
+++ app/src/main/java/com/example/studenthelpernew/SessionHandler.java
@@ -5,9 +5,7 @@ import android.content.SharedPreferences;
 
 import java.util.Date;
 
-/**
- * Created by Abhi on 20 Jan 2018 020.
- */
+
 
 public class SessionHandler {
     private static final String PREF_NAME = "UserSession";
